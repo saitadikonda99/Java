53c2a648bf5406d47dc828c07365207eadb6be80
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-09 13:33:29 +0530

    scanner

---
+++ Sumoftwonum1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sumoftwonum1 {
+	public static void main( String[] args ) {
+		Scanner input = new Scanner( System.in );
+		System.out.print("enter the number 1 : ");
+		int a = input.nextInt();
+		System.out.print("enter the number 2 : ");
+                int b = input.nextInt();
+		int sum =a+b;
+		System.out.println("sum is : " +sum );
+		}
+	}
+
