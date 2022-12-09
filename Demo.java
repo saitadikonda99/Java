3abcacee212b0269323f5a0c9be94c5353f0e24a
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-09 13:35:11 +0530

    java

--- Demo.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-	public class Demo{
-		public static void main( String[] args ) {
-			Scanner input  = new Scanner( System.in );
-			 System.out.println( input.nextLine() );
-			}
-		}
-	
