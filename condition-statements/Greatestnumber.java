a6d28ce38776146baa04161892a5c7ad10d50bd6
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-11 11:57:59 +0530

    Greatest number

---
+++ condition-statements/Greatestnumber.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Greatestnumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner( System.in );
+        System.out.println("enter the numbers :");
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        if( a>b && a>c )
+            System.out.println("Greatest number : " +a);
+        else if (b>a && b>c )
+            System.out.println("Greatest number : " +b);
+        else
+            System.out.println("Greatest number : " +c);
+
+    }
+}
+
