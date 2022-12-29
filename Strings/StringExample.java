cc2d126dd220aaee8b957845f3124e8440d07368
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-30 01:13:43 +0530

    java

---
+++ Strings/StringExample.java
@@ -0,0 +1,10 @@
+// string in java
+
+public class StringExample {
+	public static void main( String[] args ) {
+		// input the string 
+		String name = " sai ";
+		System.out.println(name);
+		}
+	}
+
---
+++ Strings/StringImmutability.java
@@ -0,0 +1,15 @@
+// StringImmutability
+
+public class StringImmutability {
+	public static void main( String[] args ) {
+		
+		String	k = "kusuma";
+		System.out.println(k);
+	// here we didn't change the object we created a new object 
+
+
+		k = "sai";
+		System.out.println(k);
+		}
+	}
+		 
