9c54ee14afed519e0128e8528233dd4c29dc04c4
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-30 01:25:40 +0530

    java

---
+++ Strings/StringComparison.java
@@ -0,0 +1,26 @@
+// string comparison in java
+
+public class StringComparison {
+	public static void main(String[] args) {
+
+         String k = "kusuma";
+         String s = "kusuma";
+         String c = k;
+
+         // ==
+
+         System.out.println( c == k );  // true
+
+         System.out.println( k == s ); // true
+
+         String k1 = new String("kusuma");
+         String s1 = new String("kusuma");
+
+         System.out.println( k1 == s1 ); // false
+
+         // it's don't care of objects it only checks the value of two strings
+
+         System.out.println(k1.equals(s1));
+
+     }
+ }
