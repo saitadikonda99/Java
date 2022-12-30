3ec1f0f476d2f0c0fd0bcbd8f4ea9cd13a9ee9f0
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-30 18:31:06 +0530

    java

---
+++ Strings/Sai.java
@@ -0,0 +1,12 @@
+// print the particular element in java
+
+	public class Sai {
+		public static void main( String[] args ) {
+		
+		String name = "kusuma";
+	
+		// print the index element 
+		System.out.println(name.charAt(0));
+		}
+	}
+	
--- Strings/StringExample.java
+++ Strings/StringExample.java
@@ -1,10 +1,13 @@
 // string in java
 
 public class StringExample {
-	public static void main( String[] args ) {
-		// input the string 
-		String name = " sai ";
-		System.out.println(name);
-		}
-	}
+    public static void main( String[] args ) {
+        // input the string
+        String name = "kusuma";
+        System.out.println(name);
+    }
+}
+
+
+
 
