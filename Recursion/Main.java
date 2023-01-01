f4eb2ac772c5a24a3f80d121f37377a13fde1066
saitadikonda99 <saiitadikonda7@gmail.com>
2023-01-01 18:14:07 +0530

    Recursion

---
+++ Recursion/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main( String[] args ) {
+		// function call
+		message();
+		}
+	static void message() {
+		System.out.println(" Hello " );
+		message2();
+		}
+	static void message2() {
+		 System.out.println(" Hello " );
+                message3();
+		}
+	 static void message3() {
+                 System.out.println(" Hello " );
+                }
+	}
+
